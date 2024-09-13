a709dc8ae7f207eb5471ca0cbd26dff6f0e3fba9
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-09-13 17:38:02 +0000

    slower than python

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # clustersort
+
+TODO:
+
+- try n_gram_range (1,2)
+- calculate 'sums' by dot product of X_tfidf with the vector of the lengths of the document collection in X
+- document_frequency min_freq=2 (absolute value, would need to make PR for linfa) -- or maybe just construct the vocab list (only include duplicate words) and document fit separately
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,179 @@
+use linfa::traits::{Fit, Predict};
+use linfa::DatasetBase;
+use linfa_clustering::KMeans;
+use linfa_preprocessing::tf_idf_vectorization::TfIdfVectorizer;
+use ndarray::{Array1, Array2};
+use ndarray_rand::rand::SeedableRng;
+use rand_xoshiro::Xoshiro256Plus;
+use regex::Regex;
+use std::collections::HashMap;
+use std::io::{self, BufRead, BufWriter, Write};
+use tracing_subscriber::fmt::format::FmtSpan;
+
+use linfa_nn::distance::LInfDist;
+use tracing::debug;
+use tracing_subscriber::fmt;
+mod word_bank;
+
+fn default_cluster_count(records: &Array2<f64>) -> usize {
+    let num_rows = records.shape()[0];
+    (num_rows as f64).sqrt().ceil() as usize
+}
+
 fn main() {
-    println!("Hello, world!");
+    setup_logging();
+
+    let stdin = io::stdin();
+    let lines: Vec<String> = stdin
+        .lock()
+        .lines()
+        .map_while(Result::ok)
+        .filter(|line| !line.trim().to_string().is_empty())
+        .collect();
+    // dbg!(&lines);
+
+    let sentence_strings = preprocess(&lines);
+    let clusters = find_clusters(sentence_strings, lines);
+    format_output(clusters);
+}
+
+fn find_clusters(sentence_strings: Vec<String>, lines: Vec<String>) -> Vec<Vec<String>> {
+    let targets = Array1::from_shape_vec(sentence_strings.len(), sentence_strings.clone()).unwrap();
+    let vectorizer = TfIdfVectorizer::default()
+        .stopwords(word_bank::STOP_WORDS)
+        .convert_to_lowercase(false)
+        .fit(&targets)
+        .unwrap();
+    debug!(
+        "We obtain a vocabulary with {} entries",
+        vectorizer.nentries()
+    );
+
+    let training_records = vectorizer.transform(&targets).to_dense();
+    debug!(
+        "We obtain a {}x{} matrix of counts for the vocabulary entries",
+        training_records.dim().0,
+        training_records.dim().1
+    );
+
+    let n_clusters = default_cluster_count(&training_records);
+    let dataset = DatasetBase::from((training_records, targets));
+    let rng = Xoshiro256Plus::seed_from_u64(42);
+    let kmeans = KMeans::params_with(n_clusters, rng, LInfDist)
+        .max_n_iterations(10)
+        .tolerance(1e-4)
+        .fit(&dataset)
+        .expect("KMeans fitted");
+
+    let predictions = kmeans.predict(dataset);
+
+    // dbg!(&predictions);
+
+    // Group lines by their cluster
+    let mut clusters: Vec<Vec<String>> = vec![Vec::new(); n_clusters];
+    for (line, cluster) in lines.iter().zip(predictions.targets().iter()) {
+        clusters[*cluster].push(line.clone());
+    }
+    clusters.sort_by_key(|cluster| cluster.len());
+    clusters
+}
+
+fn format_output(clusters: Vec<Vec<String>>) {
+    let stdout = io::stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+    for (i, cluster) in clusters.iter().enumerate() {
+        writeln!(writer, "Cluster {}:", i).unwrap();
+        for line in cluster {
+            writeln!(writer, "{}", line).unwrap();
+        }
+        writeln!(writer).unwrap();
+    }
+    writer.flush().unwrap();
+}
+
+fn setup_logging() {
+    fmt()
+        .with_target(false)
+        .with_timer(tracing_subscriber::fmt::time::uptime())
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+}
+
+fn preprocess(lines: &Vec<String>) -> Vec<String> {
+    let mut sentence_strings: Vec<String> = lines
+        .iter()
+        .map(|line| {
+            line.replace("/", " ")
+                .replace("\\", " ")
+                .replace(".", " ")
+                .replace("[", " ")
+                .replace("(", " ")
+                .replace("]", " ")
+                .replace(")", " ")
+                .replace("{", " ")
+                .replace("}", " ")
+                .replace("_", " ")
+                .replace("-", " ")
+                .to_lowercase()
+        })
+        .collect();
+
+    let word_regex = Regex::new(r"\b\w\w+\b").unwrap();
+
+    // Tokenize sentences and count word occurrences
+    let mut word_counts: HashMap<String, usize> = HashMap::new();
+    for line in &sentence_strings {
+        for word in word_regex.find_iter(&line) {
+            *word_counts.entry(word.as_str().to_string()).or_insert(0) += 1;
+        }
+    }
+
+    // Filter out unique words
+    sentence_strings = sentence_strings
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|word| {
+                    if word_counts.get(word).unwrap_or(&0) > &1 {
+                        word.to_string()
+                    } else {
+                        "".to_string()
+                    }
+                })
+                .collect::<Vec<String>>()
+                .join(" ")
+        })
+        .collect();
+
+    dbg!(sentence_strings)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_preprocess() {
+        let input = vec![
+            "hippopotamus TURTLE hippopotamus".to_string(),
+            "turtle".to_string(),
+            "squirrel!".to_string(),
+        ];
+
+        let output = preprocess(&input);
+
+        // same length
+        assert_eq!(input.len(), output.len());
+
+        // same order
+        assert_eq!(
+            output,
+            vec![
+                "hippopotamus turtle hippopotamus".to_string(),
+                "turtle".to_string(),
+                "".to_string(),
+            ]
+        );
+    }
 }
